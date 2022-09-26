699c7d3a07c241d825c1084e22054b55e8354dd0
HP Pavilion <HP Pavilion@HPPavilion15.home>
2022-09-27 00:04:32 +0100

    First commit

---
+++ src/userdefinedException/UserDefinedExceptionsEx1.java
@@ -0,0 +1,38 @@
+package userdefinedException;
+
+public class UserDefinedExceptionsEx1 {
+	
+	public static void main(String[] args) {
+		try {
+			
+			throw new MyExceptions("Exception occured");
+		
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
+
+
+class MyExceptions extends Exception{
+    
+
+	
+	
+	  private String text;
+	  
+	  MyExceptions(String text) { this.text = text; }
+	  
+		/*
+		 * // Object class is the parent class in Java. It contains the toString method.
+		 * The toString // method is used to return a string representation of an
+		 * object. If any object is printed, // the toString() method is internally
+		 * invoked by the java compiler. // Else, the user implemented or overridden
+		 * toString() method is called
+		 */
+	  
+	  public String toString() { return "Exception caught -> "+text; }
+	 
+
+}
---
+++ src/userdefinedException/UserDefinedExceptionsEx2.java
@@ -0,0 +1,42 @@
+package userdefinedException;
+
+import java.util.Scanner;
+
+public class UserDefinedExceptionsEx2 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.println("Enter a number -- ");
+		
+		int num = scn.nextInt();
+		
+		try {
+			SampleException.validateInput(num);
+		} catch (InvalidInputException e) {
+		
+			System.out.println("Caught Exception - Input is greater than 100");
+		}
+
+	}
+}
+
+class SampleException {
+    static void validateInput(int number) throws InvalidInputException
+    {
+    	if(number > 100)
+    	{
+    		throw new InvalidInputException("Limit exceeded");
+    	}
+    	System.out.println("Valid Input");
+    }
+}
+
+class InvalidInputException extends Exception{
+	InvalidInputException(String msg)
+	{
+		super(msg);
+	}
+}
+
